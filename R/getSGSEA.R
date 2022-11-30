3dd62e71e1ab510e047aa1cb2edc298e50c78b09
ShellsheDeng <118242451+ShellsheDeng@users.noreply.github.com>
2022-11-30 00:51:19 -0600

    Add files via upload
    change gsea parameter description

--- R/getSGSEA.R
+++ R/getSGSEA.R
@@ -8,8 +8,8 @@
 #' @param logHazardRatio a named list of log hazard ratios with corresponding gene symbols. The gene symbols should be unique. The list should not contain any NA's.
 #' @param minGenes the minimal number of genes that are included in a gene set for the test.
 #' @param maxGenes the maximum number of genes that are included in a gene set for the test.
-#' @param powerParam This is the power, to which all input gene-level stats are raised to. If gseaParam is zero, then all gene-level stats effectively become ones and GSEA test
-#' becomes Kolmogorov-Smirnov test. The default value is 1, meaning that gene-level stats are used as is. Also see gseaParam in fgsea function from fgsea package.
+#' @param powerParam The default value is 1, meaning that gene-level stats are used as it's. If gseaParam is 0, then all gene-level stats effectively become ones
+#' and GSEA test becomes Kolmogorov-Smirnov test. The power of the test that all input gene-level stats are raised to. Also see gseaParam in fgsea function from fgsea package.
 
 
 #' @import BiocManager
