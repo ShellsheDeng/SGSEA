969d381070d73faf98be7ea5c4f86c385a90ad2f
ShellsheDeng <shellshe_deng@hotmail.com>
2023-08-05 23:15:28 -0500

    change descriptions for getLHR and getLFC

--- R/getLHR.R
+++ R/getLHR.R
@@ -1,12 +1,10 @@
-#' Generate log hazard ratios from Cox Proportional Hazards Model
-#' @description Return a list of log hazard ratios with corresponding gene symbols as their names. This function is calling coxph function from survival package.
-#' @param normalizedData a data frame contains normalized counts. Columns should be the gene symbols and rows should be sample IDs.
-#' Data should also be filtered before entering this step.
-#' @param survTime a numeric vector of survival times. User should be aware of keeping the same order of gene symbols for survival times and the genes in normalizedData.
-#' @param survStatus a numeric vector indicating events. Normally 0=alive, 1=dead. Or 1=alive, 2=death. If omitted, all subjects are assumed to have an event.
-#' User should be aware of keeping the same order of gene symbols for survival status and the genes in normalizedData.
+#' Generate a numeric vector of the log hazard ratios from Cox Proportional Hazards Model
+#' @description Return a numeric vector of log hazard ratios with corresponding gene symbols as their names. This function is calling coxph function from the survival package.
+#' @param normalizedData A data frame contains the normalized counts. Rows must be the sample IDs. Columns must be the unique gene symbols. Data should be filtered and normalized before entering this step. See KIRC data included in this package as the input example format.
+#' @param survTime A numeric vector of patient's survival times. User should be aware of matching the gene symbols from the normalized count data with each individual's survival times.
+#' @param survStatus A numeric vector of patient's survival status. Normally 0=alive, 1=dead. Or 1=alive, 2=death. If omitted, all subjects are assumed to have an event. User should be aware of matching the gene symbols from the normalized count data with each individual's survival status.
 #' @import survival
-#' @return a named list of log hazard ratios with corresponding gene symbols.
+#' @return A named list of the log hazard ratios with their corresponding gene symbols.
 #' @export
 
 
