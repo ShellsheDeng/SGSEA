35a0cf880ccb4fa9b6bb51cd9e62bcd733a35a85
ShellsheDeng <shellshe_deng@hotmail.com>
2023-08-05 21:10:56 -0500

    change the description for sample data

--- R/data.R
+++ R/data.R
@@ -1,16 +1,17 @@
-
 #' Kidney Renal clear cell carcinoma(KIRC)
 #'
 #' A subset of Kidney Renal Clear Cell Carcinoma mRNA seq data (version 2016_01_28) from Broad Institute The Cancer Genome Atlas (TCGA) Genome Data Analysis Center (GDAC) Firehose website (http://gdac.broadinstitute.org).
 #'
-#' @format ## `KIRC`
-#' A data frame with 520 patient IDs (rows) and 3000 gene symbols (columns) :
+#' @format A data frame with 520 rows (patient ID) and 3000 variables (gene symbols):
 #' \describe{
 #'   \item{ID}{patient's ID}
 #'   \item{survtime}{patient's survival time in days}
 #'   \item{status}{1 for alive, 2 for dead}
 #'   \item{barcode}{patient's barcode}
+#'   \item{A1BG}{gene symbol}
 #'   ...
 #' }
 #' @source <https://gdac.broadinstitute.org/#>
+#' @examples
+#' data(KIRC)  # lazy loading.
 "KIRC"
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Survaival-based Gene Set Enrichment Analysis
-This R package currently contains 7 functions ("getNORM","getLHR","getLFC","getReactome","getSGSEA","getEnrichPlot" and "getTOP10") and 1 data set named "KIRC" for users to try example code.
+This R package currently contains 7 functions ("getNORM","getLHR","getLFC","getReactome","getSGSEA","getEnrichPlot" and "getTOP10") and 1 data set named "KIRC" for users to learn what the input data format should be.
 
 Download this package from GitHub: 
 1. install BiocManager and devtools
