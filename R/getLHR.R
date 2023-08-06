9d068b56d95fb9dd08616bb2a45f6b494f1d32bb
ShellsheDeng <shellshe_deng@hotmail.com>
2023-08-05 23:21:59 -0500

    change the descriptions

--- R/getLHR.R
+++ R/getLHR.R
@@ -1,6 +1,6 @@
 #' Generate a numeric vector of the log hazard ratios from Cox Proportional Hazards Model
-#' @description Return a numeric vector of log hazard ratios with corresponding gene symbols as their names. This function is calling coxph function from the survival package.
-#' @param normalizedData A data frame contains the normalized counts. Rows must be the sample IDs. Columns must be the unique gene symbols. Data should be filtered and normalized before entering this step. See KIRC data included in this package as the input example format.
+#' @description Return a numeric vector of log hazard ratios with corresponding gene symbols as their names. This function is calling coxph function from the survival package. Data should be filtered and normalized before entering this step.
+#' @param normalizedData A data frame contains the normalized counts. Rows must be the sample IDs. Columns must be the unique gene symbols. See KIRC data included in this package as the input example format.
 #' @param survTime A numeric vector of patient's survival times. User should be aware of matching the gene symbols from the normalized count data with each individual's survival times.
 #' @param survStatus A numeric vector of patient's survival status. Normally 0=alive, 1=dead. Or 1=alive, 2=death. If omitted, all subjects are assumed to have an event. User should be aware of matching the gene symbols from the normalized count data with each individual's survival status.
 #' @import survival
--- R/getNORM.R
+++ R/getNORM.R
@@ -1,11 +1,11 @@
-#' get normalized data
-#' @description This function uses mean-variance method to get the normalized values for a count data. It's calling voom function from limma package.
-#' @param countsData a data frame contains all numeric counts. Columns should be the gene symbols and rows should be sample IDs.
+#' Normalized the raw counts data
+#' @description This function uses mean-variance method to get a normalized count data. It's calling voom function from the limma package. Data should be filtered before entering this step.
+#' @param countsData A data frame contains all numeric counts. Rows must be the sample IDs. Columns must be the unique gene symbols. See KIRC data included in this package as the input example format.
 #' @import limma
-#' @return return the same data frame format with normalized counts
+#' @return Return a data frame with normalized counts.
 #' @export
 
-getNORM <- function (countsData) {
+getNorm <- function (countsData) {
 
   if(!(is.data.frame(countsData))) {
     stop(paste0(countsData, 'must be a data frame'))
